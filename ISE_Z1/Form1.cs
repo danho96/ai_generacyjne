80d5575647e8415558edcaa40764b523e56b8492
MateuszNycz <mateusz.gsm@gmail.com>
2020-05-16 17:48:17 +0200

    Refactor

--- ISE_Z1/Form1.cs
+++ ISE_Z1/Form1.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Text;
+using System.Linq;
 using System.Windows.Forms;
 using ZedGraph;
 namespace ISE_Z1
@@ -17,49 +16,28 @@ namespace ISE_Z1
         //kolory poszczególnych przebiegów
         public List<Color> populationColor = new List<Color>();
 
-        public Form1()
-        {
-            InitializeComponent();
-            graph.GraphPane.Title.IsVisible = false;
-            CreateBasicFunction();
-        }
-
-        /// <summary>
-        /// Inicjalizuje funkcje
-        /// </summary>
-        private void CreateBasicFunction()
-        {
-            //wpsolczynniki y = ax + b (TSI)
-            //klasa Random rand = new Random
-            // metoda next() daje wartosc z przedzialu 0,1
-            //metoda split() split(new String[]{" "}, stringsplitoptionsremoveemptyetries);
-            //klasa Double.
-            population.Add("0,05");
-            population.Add("0,010 0,0014 0,0003 0,0006");
-            population.Add("0,004 0,00002 0,000008 0,00000055");
-            population.Add("0,00003 0,02 0,008 0,009 0,00003 ");
-            population.Add("0,0000000006 0,000000001 0,008 0,009 0,00003 0,55");
-            population.Add("0,0000000003 0,0000000002 0,008 0,009 0,00003 0,1 ");
-
-            populationColor.Add(Color.HotPink);
-            populationColor.Add(Color.IndianRed);
-            populationColor.Add(Color.Khaki);
-            populationColor.Add(Color.LightBlue);
-            populationColor.Add(Color.Gray);
-            populationColor.Add(Color.Gold);
-            populationColor.Add(Color.DarkGreen);
-            populationColor.Add(Color.Blue);
-            populationColor.Add(Color.DarkSlateBlue);
-            populationColor.Add(Color.Orange);
-
-        }
-
         //iloœæ punktów na osi x
         int pointAmount = 1000;
         //generator liczb pseudolosowych
         Random rand = new Random(1000);
         //lista punktów przebiegu do którego dopasowujemy
         PointPairList ppl = new PointPairList();
+
+        public Form1()
+        {
+            InitializeComponent();
+            graph.GraphPane.Title.IsVisible = false;
+            InitializePopulation(population, populationColor);
+        }
+
+        /// <summary>
+        /// Inicjalizuje funkcje
+        /// </summary>
+        private void InitializePopulation(List<string> population, List<Color> colors)
+        {
+            AddPopulation(population);
+            AddColors(colors);
+        }
 
         //tworzy funkcjê do której dopasowujemy
         private void buttonCreateFunction_Click(object sender, EventArgs e)
@@ -94,43 +72,38 @@ namespace ISE_Z1
 
         private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
-            for (int c = 0; c < 1; c++)
+            //wyczyœæ wykres
+            graph.GraphPane.CurveList.Clear();
+            //dodaj przebieg funkcji do której dopasowujemy
+            graph.GraphPane.AddCurve("Wykres", ppl, Color.Red, SymbolType.None);
+            CreateGeneration();
+            //dodaj wykresy fukcji dopasowywanych
+
+            for (int i = 0; i < population.Count; i++)
             {
-                //wyczyœæ wykres
-                graph.GraphPane.CurveList.Clear();
-                //dodaj przebieg funkcji do której dopasowujemy
-                graph.GraphPane.AddCurve("Wykres", ppl, Color.Red, SymbolType.None);
-                CreateGeneration();
-                //dodaj wykresy fukcji dopasowywanych
-                
-                for (int i = 0; i < population.Count; i++)
-                {
-                    //pobierz wspó³czynniki funkji
-                    string[] coefficientString = population[i].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-                    List<double> coefficient = new List<double>();
-                    for (int j = 0; j < coefficientString.Length; j++)
-                    {
-                        coefficient.Add(Convert.ToDouble(coefficientString[j]));
-                    }
+                //pobierz wspó³czynniki funkji
+                var coefficient = population[i]
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => Convert.ToDouble(x))
+                    .ToList();
 
-                    PointPairList popPairList = new PointPairList();
-                    //stwórz przebieg
-                    for (int j = 0; j < pointAmount; j++)
-                    {
-                        popPairList.Add(j, GetYValue(coefficient, j));
-                    }
-                    //dodaj do listy
-                    graph.GraphPane.AddCurve(i.ToString(), popPairList, populationColor[i], SymbolType.None);
-                }
-
-                if (((BackgroundWorker)sender).CancellationPending)
+                PointPairList popPairList = new PointPairList();
+                //stwórz przebieg
+                for (int j = 0; j < pointAmount; j++)
                 {
-                    e.Cancel = true;
-                    return;
+                    popPairList.Add(j, GetYValue(coefficient, j));
                 }
-                //odœwie¿
-                RefreshGraph();
+                //dodaj do listy
+                graph.GraphPane.AddCurve(i.ToString(), popPairList, populationColor[i], SymbolType.None);
             }
+
+            if (((BackgroundWorker)sender).CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+            //odœwie¿
+            RefreshGraph();
         }
 
         private void CreateGeneration()
@@ -139,41 +112,25 @@ namespace ISE_Z1
             //TODO wype³niæ
             Random rand = new Random();
 
-            int populationBase = population.Count;
             List<double> tablicaBledow = new List<double>();
 
             for (int k = 0; k < 10; k++)
             {
-                int firstSelector = rand.Next() % populationBase;
-                String first = population[firstSelector];
-                String second = population[rand.Next() % populationBase];
+                var firstPopulation = GetRandomPopulation();
+                var secondPopulation = GetRandomPopulation();
+                var size = firstPopulation.Length > secondPopulation.Length ? firstPopulation.Length : secondPopulation.Length;
 
-                string[] firstString = first.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-                string[] secondString = first.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-
-                int size = firstString.Length > secondString.Length ? firstString.Length : secondString.Length;
-
-                double[] firstDoubles = new double[firstString.Length];
-                for (int i = 0; i < firstString.Length; i++)
-                {
-                    firstDoubles[i] = Convert.ToDouble(firstString[i]);
-                }
-                double[] secondDoubles = new double[secondString.Length];
-                for (int i = 0; i < secondString.Length; i++)
-                {
-                    secondDoubles[i] = Convert.ToDouble(secondString[i]);
-                }
                 string[] child = new string[size];
                 Double a;
                 List<double> coeficients = new List<double>();
                 for (int j = 0; j < size; j++)
                 {
-                    int dzialanie = rand.Next() % 6;
+                    int dzialanie = rand.Next(0, 5);
                     double m;
                     switch (dzialanie)
                     {
                         case 0:
-                            a = (firstDoubles[j % firstDoubles.Length] + secondDoubles[j % secondDoubles.Length]) / 2;
+                            a = (firstPopulation[j % firstPopulation.Length] + secondPopulation[j % secondPopulation.Length]) / 2;
                             if ((m = rand.Next()) < 0.1)
                             {
                                 a += m;
@@ -182,7 +139,7 @@ namespace ISE_Z1
                             coeficients.Add(a);
                             break;
                         case 1:
-                            a = firstDoubles[j % firstDoubles.Length] + secondDoubles[j % secondDoubles.Length];
+                            a = firstPopulation[j % firstPopulation.Length] + secondPopulation[j % secondPopulation.Length];
                             if ((m = rand.Next()) < 0.1)
                             {
                                 a += m;
@@ -200,7 +157,7 @@ namespace ISE_Z1
                             coeficients.Add(a);
                             break;
                         case 3:
-                            a = firstDoubles[j % firstDoubles.Length] - secondDoubles[j % secondDoubles.Length];
+                            a = firstPopulation[j % firstPopulation.Length] - secondPopulation[j % secondPopulation.Length];
                             if ((m = rand.Next()) < 0.1)
                             {
                                 a += m;
@@ -209,7 +166,7 @@ namespace ISE_Z1
                             coeficients.Add(a);
                             break;
                         case 4:
-                            a = firstDoubles[j % firstDoubles.Length];
+                            a = firstPopulation[j % firstPopulation.Length];
                             if ((m = rand.Next()) < 0.1)
                             {
                                 a += m;
@@ -218,7 +175,7 @@ namespace ISE_Z1
                             coeficients.Add(a);
                             break;
                         case 5:
-                            a = secondDoubles[j % firstDoubles.Length];
+                            a = secondPopulation[j % secondPopulation.Length];
                             if ((m = rand.Next()) < 0.1)
                             {
                                 a += m;
@@ -239,7 +196,6 @@ namespace ISE_Z1
 
                 SortedDictionary<double, String> mapaDoWyboruNajlepszych = new SortedDictionary<double, String>();
                 mapaDoWyboruNajlepszych.Add(sum, String.Join(" ", child));
-                //do niej przepisuje posortowane? wartosci z mapy 
 
                 foreach (KeyValuePair<double, String> pair in mapaDoWyboruNajlepszych)
                 {
@@ -281,5 +237,37 @@ namespace ISE_Z1
             }
         }
         private delegate void RefreshGraphDelegate();
+
+        private void AddColors(List<Color> populationColor)
+        {
+            populationColor.Add(Color.HotPink);
+            populationColor.Add(Color.IndianRed);
+            populationColor.Add(Color.Khaki);
+            populationColor.Add(Color.LightBlue);
+            populationColor.Add(Color.Gray);
+            populationColor.Add(Color.Gold);
+            populationColor.Add(Color.DarkGreen);
+            populationColor.Add(Color.Blue);
+            populationColor.Add(Color.DarkSlateBlue);
+            populationColor.Add(Color.Orange);
+        }
+
+        private void AddPopulation(List<string> population)
+        {
+            population.Add("0,05");
+            population.Add("0,010 0,0014 0,0003 0,0006");
+            population.Add("0,004 0,00002 0,000008 0,00000055");
+            population.Add("0,00003 0,02 0,008 0,009 0,00003 ");
+            population.Add("0,0000000006 0,000000001 0,008 0,009 0,00003 0,55");
+            population.Add("0,0000000003 0,0000000002 0,008 0,009 0,00003 0,1 ");
+        }
+
+        private double[] GetRandomPopulation()
+        {
+            return population[rand.Next() % population.Count]
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => Convert.ToDouble(x))
+                .ToArray();
+        }
     }
 }
